2e400f36398c87c0b22d9e258d7424732fa4f1c7
Megapixel-code <77630002+Megapixel-code@users.noreply.github.com>
2022-05-10 11:38:03 +0200

    dfqskljidfssgklfq voila

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
-<include stdio.h>
+#include <stdio.h>
+#define SIZE 9 //DO NOT USE IN FUNCTIONS
+
+//BombMap is the map with only the bomb emplacement marked as -1
+//and a numbers for the amont of bombs around
+
+//userMap is the map that the user see with -1 to the unrevealed terrain
+
+
+void display(int bMap[], int uMap[], int size){
+	printf("   ");
+	for (int i = 0; i<size; i++){
+		printf("%c ", i+65);
+	}
+}
+
 
 int main(){
-  return 0;
+	int bombMap[SIZE][SIZE];
+	int userMap[SIZE][SIZE];
+	display(bombMap, userMap, SIZE);
+	return 0;
 }
